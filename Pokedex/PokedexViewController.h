241b59ea6247a0a19b40b8c4178e4ddaad8bd5a4
Daniel Sánchez Seijo <daniseijo12@gmail.com>
2014-04-03 12:30:03 +0200

    Cambio final de carpeta

---
+++ Pokedex/PokedexViewController.h
@@ -0,0 +1,13 @@
+//
+//  PokedexViewController.h
+//  Pokedex
+//
+//  Created by Daniel Sánchez Seijo on 03/04/14.
+//  Copyright (c) 2014 g102 DIT UPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PokedexViewController : UIViewController
+
+@end
---
+++ Pokedex/PokedexViewController.m
@@ -0,0 +1,29 @@
+//
+//  PokedexViewController.m
+//  Pokedex
+//
+//  Created by Daniel Sánchez Seijo on 03/04/14.
+//  Copyright (c) 2014 g102 DIT UPM. All rights reserved.
+//
+
+#import "PokedexViewController.h"
+
+@interface PokedexViewController ()
+
+@end
+
+@implementation PokedexViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
